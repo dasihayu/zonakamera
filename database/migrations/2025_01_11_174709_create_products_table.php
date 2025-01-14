df6c119248a28f1ca3c6ea2e632a714309790203
dasihayu <dasihayu1307@gmail.com>
2025-01-14 12:52:37 +0700

    chore: update product table

--- database/migrations/2025_01_11_174709_create_products_table.php
+++ database/migrations/2025_01_11_174709_create_products_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->smallInteger('sort')->default(0)->index();
             $table->boolean('is_visible')->default(false)->index();
             $table->string('title', 255)->nullable();
             $table->decimal('price', 12, 2)->nullable();
             $table->text('description')->nullable();
             $table->string('image_url', 255)->nullable();
-            $table->string('slug')->nullable()->unique();
             $table->timestamps();
             $table->softDeletes();
         });
