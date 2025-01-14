a40816b8289860a8549f4e7f125723d7737d6e3f
dasihayu <dasihayu1307@gmail.com>
2025-01-14 10:18:16 +0700

    feat: make migration and create product category resource

---
+++ app/Filament/Resources/ProductcategoryResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductcategoryResource\Pages;
+use App\Filament\Resources\ProductcategoryResource\RelationManagers;
+use App\Models\Productcategory;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductcategoryResource extends Resource
+{
+    protected static ?string $model = Productcategory::class;
+
+    protected static ?string $navigationIcon = 'fluentui-stack-20';
+
+    protected static ?string $navigationLabel = 'Categories';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    protected static ?string $pluralModelLabel = 'Categories';
+    
+    protected static ?string $modelLabel = 'Category';
+
+    protected static ?int $navigationSort = -1;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->live(onBlur: true),
+
+                Forms\Components\Toggle::make('is_active')
+                    ->default(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('is_active')->label('Status')
+                    ->badge()
+                    ->alignCenter()
+                    ->formatStateUsing(fn(string $state, $record) => match ($state) {
+                        '0' => 'Not Active',
+                        '1' => 'Active',
+                    })
+                    ->color(fn(string $state): string => match ($state) {
+                        '0' => 'danger',
+                        '1' => 'success',
+                    }),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProductcategories::route('/'),
+            'create' => Pages\CreateProductcategory::route('/create'),
+            'edit' => Pages\EditProductcategory::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __("menu.nav_group.product");
+    }
+}
---
+++ database/migrations/2025_01_11_174709_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('sort')->default(0)->index();
+            $table->boolean('is_visible')->default(false)->index();
+            $table->string('title', 255)->nullable();
+            $table->decimal('price', 12, 2)->nullable();
+            $table->text('description')->nullable();
+            $table->string('image_url', 255)->nullable();
+            $table->string('slug')->nullable()->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
