e8df7cb83983604b6b8940e3e12de405a71a93c4
dasihayu <dasihayu1307@gmail.com>
2025-01-26 01:28:35 +0700

    chore: update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             UsersTableSeeder::class,
+            ProductSeeder::class
         ]);
 
         Artisan::call('shield:generate --all');
