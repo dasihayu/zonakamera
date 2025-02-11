da01b54e6ccae70aaf853d7f05dcc01e1407e565
dasihayu <dasihayu1307@gmail.com>
2025-02-11 10:00:05 +0700

    fix: correct namespace reference for ProductCategory model

--- app/Filament/Resources/ProductcategoryResource.php
+++ app/Filament/Resources/ProductcategoryResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ProductcategoryResource\Pages;
 use App\Filament\Resources\ProductcategoryResource\RelationManagers;
-use App\Models\Productcategory;
+use App\Models\ProductCategory;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
