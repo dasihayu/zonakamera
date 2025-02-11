05cb43dc815376a088b2505d5da20e6e1e5224b9
dasihayu <dasihayu1307@gmail.com>
2025-02-11 14:24:45 +0700

    fix: allow username field to be nullable in users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('username')->unique();
+            $table->string('username')->unique()->nullable();
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
